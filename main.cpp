c3a8a358ca67aa19c9279c6e653a2789578a7b36
Fukuda097548205980248520795274 <k024g0083@g.neec.ac.jp>
2025-04-22 21:48:52 +0900

    main内の処理を消した

--- main.cpp
+++ main.cpp
@@ -7,47 +7,6 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int)
 	engine->Initialize(1280, 720);
 
 
-
-
-	// カメラ
-	Transform3D cameraTransform =
-	{
-		{1.0f , 1.0f , 1.0f},
-		{0.0f , 0.0f ,0.0f},
-		{0.0f , 0.0f , -10.0f}
-	};
-
-	// 光源
-	DirectionalLight light
-	{
-		{1.0f , 1.0f , 1.0f , 1.0f},
-		{0.0f , -1.0f , 0.0f},
-		1.0f
-	};
-
-	// 三角形
-	Transform3D triangle
-	{
-		{1.0f , 1.0f , 1.0f},
-		{0.0f , 0.0f , 0.0f},
-		{0.0f , 0.0f , 0.0f},
-	};
-
-	// テクスチャ
-	uint32_t ghUvChecker = engine->LoadTexture("Resources/Textures/uvChecker.png");
-	uint32_t ghMonsterBall = engine->LoadTexture("Resources/Textures/monsterBall.png");
-
-	// モデル
-	uint32_t model = engine->LoadModelData("Resources/ModelDatas", "axis.obj");
-
-	// サウンド
-	uint32_t shAlarm01 = engine->LoadSound("Resources/Sounds/Se/Alarm01.wav");
-	uint32_t shAlarm02 = engine->LoadSound("Resources/Sounds/Se/Alarm02.wav");
-	uint32_t shAlarm03 = engine->LoadSound("Resources/Sounds/Se/Alarm03.wav");
-
-	engine->PlayerSoundWav(shAlarm01);
-
-
 	// メインループ
 	while(engine->IsWindowOpen())
 	{
@@ -58,25 +17,6 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int)
 		/// ↓ 更新処理ここから
 		/// 
 
-		ImGui::Begin("Triangle");
-		ImGui::DragFloat3("cmaera Translate", &cameraTransform.translate.x , 0.01f);
-		ImGui::DragFloat3("rotation", &triangle.rotate.x, 0.01f);
-		ImGui::End();
-
-		Matrix4x4 viewMatrix = Make4x4InverseMatrix(Make4x4AffineMatrix(cameraTransform.scale, cameraTransform.rotate, cameraTransform.translate));
-		Matrix4x4 projectionMatrix = Make4x4PerspectiveFovMatrix(0.45f, 1280.0f / 720.0f, 0.1f, 100.0f);
-		Matrix4x4 orthoMatrix = Make4x4OrthographicsMatrix(0.0f, 0.0f, 1280.0f, 720.0f, 0.0f, 100.0f);
-
-		if (engine->PushTriggerKeys(DIK_A))
-		{
-			engine->PlayerSoundWav(shAlarm02);
-		}
-
-		if (engine->PushTriggerKeys(DIK_D))
-		{
-			engine->PlayerSoundWav(shAlarm03);
-		}
-
 		///
 		/// ↑ 更新処理ここまで
 		/// 
@@ -84,8 +24,6 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int)
 		///
 		/// ↓ 描画処理ここから
 		/// 
-		engine->DrawModel(model, triangle, Multiply(viewMatrix, projectionMatrix), light);
-		
 
 		///
 		/// ↑ 描画処理ここまで
